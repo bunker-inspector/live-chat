d8babdfe5e707456eb7be97380be25e6283c8217
Ted Kassen <tedgkassen@gmail.com>
2017-07-05 20:22:52 -0700

    Ugh... frontend

--- js/home.js
+++ js/home.js
@@ -1,15 +1,30 @@
+var USER = 'user'
+
 $(document).ready(function () {
-  const dataView = $('#data-view')
-
-  dataView.hide()
-
-  $.get('http://localhost:8080/data', (data) => {
-      dataView.text(JSON.stringify(data, null, 2))
-  });
-
-  $('#data-disp-btn').click((e) => { dataView.toggle() })
-  $('#data-dnld-btn').click((e) => {
-    e.preventDefault()
-    window.location.href = 'http://localhost:8080/download'
-  })
+  window.addEventListener("beforeunload", signOut)
 });
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+
+  let userData = {
+    name: profile.getName(),
+    googleId: profile.getId()
+  }
+
+  sessionStorage.setItem(USER, userData)
+
+  $.post('http://localhost:8080/join', userData)
+    .done((data) => {
+
+    })
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    sessionStorage.removeItem(USER)
+    console.log('User signed out.')
+  })
+}
