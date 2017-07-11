5e9e32f8f5998eb2da77330e02fd5f450ecf4bb3
Kassen, Ted <Ted_Kassen@intuit.com>
2017-07-10 19:58:45 -0700

    update video

--- js/home.js
+++ js/home.js
@@ -26,6 +26,7 @@ function updateVideo() {
     $('#video-view').attr('src', `https://www.youtube.com/embed/${data}?autoplay=1`)
     $('#chat-view').attr('src', `https://www.youtube.com/live_chat?v=${data}&embed_domain=${document.domain}`)
   })
+}
 
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
