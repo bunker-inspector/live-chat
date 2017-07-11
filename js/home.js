f879f575f70c56564a0fc96f2f57909651478575
Kassen, Ted <Ted_Kassen@intuit.com>
2017-07-10 19:13:29 -0700

    autoplay

--- js/home.js
+++ js/home.js
@@ -22,7 +22,7 @@ function displayUserMessages(id, name) {
 }
 
 function updateVideo(data) {
-  $('#video-view').attr('src', `http://www.youtube.com/embed/${data}`)
+  $('#video-view').attr('src', `http://www.youtube.com/embed/${data}?autoplay=1`)
   $('#chat-view').attr('src', `https://www.youtube.com/live_chat?v=${data}?embed_domain=${document.domain}`)
 }
 
